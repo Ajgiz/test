ee488dcd120a6bb793610c96ec147157fbedcdb3
Drophaus <doufullin@gmail.com>
2022-05-01 15:38:22 +0500

    secondary

--- client/src/request.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3001/api";
-
-export const request = axios.create({
-    baseURL: baseUrl,
-    withCredentials: true,
-});
--- server/src/db.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from '@sequelize/core'
-
-const sequelize = new Sequelize("Contact-book", "postgres", "demion2289", {
-  dialect: "postgres",
-  host: "localhost",
-  port: 5432,
-});
-
-export default sequelize;
